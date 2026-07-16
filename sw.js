160fcc13d41b0d7d998abb3342e9b4088970b011
njf520 <njf520@hotmail.com>
2026-07-16 09:59:06 -0700

    Redesign Premium/Repeat button icons and the Premium modal

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 //
 // Bump CACHE_NAME on every deploy so old caches get cleaned up and clients
 // pick up fresh assets.
-const CACHE_NAME = 'airtime-v3.20.0';
+const CACHE_NAME = 'airtime-v3.21.0';
 const APP_SHELL = [
   './',
   './index.html',
